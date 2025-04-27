8eb279703e39cc880aa7862f28f452f688480f8a
Дьяконов Иван <hgfgvvg.ru@gmail.com>
2025-04-27 20:10:22 +0300

    Task 3 complited

--- main.cpp
+++ main.cpp
@@ -4,16 +4,34 @@ using namespace std;
 
 
 int main() {
-    ofstream outfile;
-    string n;
-    getline(std::cin, n);
-   for (int i = 0; i < n.length(); i++) {
-       if (n[i]==' '){
-           n[i] = '_';
-       }
-   }
-    cout << n << endl;
+    while (true) {
+        int number;
+        cout << "Please enter a number of exersice: ";
+        cin >> number;
+        if (number == 1) {
+            ofstream outfile;
+            string n;
+            getline(cin, n);
+            outfile.open(n);
+            while (true) {
+                string s="";
+                cout << "Enter you string: " << endl;
+                getline(cin, s);
+                if (s=="stop"){break;}
+                outfile << s << endl;
+            }
+        }
+        if (number == 2) {
 
+        }
+        if (number != 1 and number != 2) {
+            cout << "Error! Enter a valid number!" << endl;
+        }
 
+        string input;
+        cout << endl << "Stop y/n";
+        cin >> input;
+        if (input == "y" or input == "Y") break;
+    }
     return 0;
 }
